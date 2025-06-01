3e70f9d87f9b405a33608021fe70e019f202f8cb
BETRAND <mmutagha@gmail.com>
2025-05-31 20:46:03 -0400

    update

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,9 +1,9 @@
 def call(String imageName = 'my-image:latest', String credsId = 'dockerhub-creds-id') {
     withCredentials([usernamePassword(credentialsId: credsId, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh '''
-            echo "$PASS" | docker login -u "$USER" --password-stdin
+        sh """
+            echo "\$PASS" | docker login -u "\$USER" --password-stdin
             docker push ${imageName}
             docker logout
-        '''
+        """
     }
 }
